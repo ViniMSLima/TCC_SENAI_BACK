fcc9d23bda3a7ef38ac840f8d92d858023046630
luizblank <luizrosacardoso@gmail.com>
2024-09-05 10:39:34 -0300

    sasds

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -325,7 +325,7 @@ class UserController {
             birthdate,
             adm,
             sex,
-            BoschID: CryptoJS.AES.encrypt(BoschID, process.env.SECRET).toString(),
+            BoschID: BoschID,
             password: encrypted,
             email,
             cep,
@@ -405,6 +405,7 @@ class UserController {
                 if (encrypted[i] == "/")
                     encrypted[i] = "$";
             }
+            console.log(encrypted)
             console.log(1)
             const transporter = nodemailer.createTransport({
                 service: 'gmail',
